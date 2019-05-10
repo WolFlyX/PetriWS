71cd12e98f9ffbf885d5b0e9112e315c759085b1
WolFlyX <44460864+WolFlyX@users.noreply.github.com>
2019-05-10 10:37:08 +0200

    Add files via upload

---
+++ jpu2016_javapetri_BDD/src/JavaPetriMain.java
@@ -0,0 +1,8 @@
+import jpu2016.javapetri.controller.JavaPetriController;
+
+public abstract class JavaPetriMain {
+
+	public static void main(final String[] args) {
+		new JavaPetriController().start();
+	}
+}
